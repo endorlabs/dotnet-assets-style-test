9b2c9a9b3fc13cb2aa0267f7bb96aae38853809e
Anand Upadhyay <anand.upadhayay@endor.ai>
2023-10-10 10:02:25 +0530

    removing all the obj and adding gitignore for it

---
+++ import/import.cs
@@ -0,0 +1,12 @@
+using System;
+namespace testimport
+{
+    public class Import
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello Import");
+        }
+    }
+}
+
---
+++ ref/ref.cs
@@ -0,0 +1,12 @@
+using System;
+namespace testRef
+{
+    public class Ref
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello Ref");
+        }
+    }
+}
+
